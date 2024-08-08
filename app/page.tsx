76b281102242c80c2e958aa957e188466bdce596
ceavinrufus <18221162@std.stei.itb.ac.id>
2024-08-08 11:31:24 +0700

    feat: discover

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Articles from "@/components/Articles";
 import BeyondPremium from "@/components/BeyondPremium";
+import Discover from "@/components/Discover";
 import Hero from "@/components/Hero";
 import Products from "@/components/Products";
 import { fetchProducts, fetchArticles } from "@/server/actions";
@@ -8,6 +9,7 @@ import {
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
+import Image from "next/image";
 
 export default async function Home() {
   const queryClient = new QueryClient();
@@ -29,6 +31,18 @@ export default async function Home() {
       <HydrationBoundary state={dehydrate(queryClient)}>
         <Hero />
         <BeyondPremium />
+        <Discover />
+        <section id="separator" className="p-4 w-full max-w-7xl mx-auto">
+          <Image
+            src={`/separator-color.svg`}
+            width={1096}
+            height={97}
+            loading="lazy"
+            className="object-contain object-center w-full"
+            alt="Discover Tailored Experiences"
+            sizes="100vw"
+          />
+        </section>
         {/* <Products />
         <Articles /> */}
       </HydrationBoundary>
